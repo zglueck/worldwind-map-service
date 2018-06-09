5dd04e9e47d05f947df77288f5c78e11c13d2498
Zach Glueckert <zachrg@gmail.com>
2018-06-08 22:52:08 -0500

    Switch to webpack build

---
+++ test/MapService.test.js
@@ -0,0 +1,14 @@
+define(['../lib/MapService'], function (MapService) {
+
+    describe("initial test", function () {
+        it("should work", function () {
+            console.log('The MapService looks like:');
+            console.log(MapService);
+            // const mapService = new MapService();
+            // const serviceAddress = 'https://worldwind25.arc.nasa.gov/wms';
+            // const url = mapService.buildWmsUrl(serviceAddress);
+            fail("do something");
+            // expect(url).toBe('https://worldwind25.arc.nasa.gov/wms?service=WMS&request=GetCapabilities');
+        });
+    });
+});
